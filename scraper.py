db8605b4b896ced764c5aa28f7ec2de1e0115596
Trevor Specht <trevor.specht@mapbox.com>
2020-10-29 20:35:30 -0400

    works for name and link

--- scraper.py
+++ scraper.py
@@ -7,10 +7,23 @@ import csv
 url = 'https://www.willdrinkfortravel.com/posts/black-owned-restaurants-you-need-to-visit-in-baltimore'
 response = requests.get(url)
 
-soup = BeautifulSoup(response.text, "html.parser")
-data = soup.find_all('p')
-# print(soup.find_all('p'))
-# print(soup.get_text())
-with open('restaurants.csv', mode='w') as restaurants:
+soup = BeautifulSoup(response.text, "lxml")
+divs = soup.find_all('div')
+links = soup.find_all('a')
+listings = soup.find_all('li')
+
+# p = soup.find('p')
+# p = soup.p['class']
+# a = soup.a['href']
+# for div in soup.find_all('div'):
+#   if div.get('id') == 'item-5e4184b6e6d48704f1813b9b':
+#     listings = div
+restaurant = []
+with open('restaurants.csv', mode='w', newline='') as restaurants:
   rest_writer = csv.writer(restaurants, delimiter=',', quotechar='"')
-  rest_writer.writerow(data)
+  for i, listing in enumerate(soup.find_all('li')):
+    # restaurant.append(listing) - makes an array of listings
+    tag = listing.a
+    name = tag.string
+    link = listing.a.get('href')
+    rest_writer.writerow([name, link])
