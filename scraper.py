357809a32961b5eb900b672a357fe9d726f42afa
Trevor Specht <trevor.specht@mapbox.com>
2020-11-02 00:09:54 -0500

    regx that isolates address

--- scraper.py
+++ scraper.py
@@ -3,29 +3,28 @@ import urllib.request
 import time
 from bs4 import BeautifulSoup
 import csv
+import re
 
 url = 'https://www.willdrinkfortravel.com/posts/black-owned-restaurants-you-need-to-visit-in-baltimore'
 response = requests.get(url)
-
 soup = BeautifulSoup(response.text, "lxml")
-divs = soup.find_all('div')
-links = soup.find_all('a')
-listings = soup.find_all('li')
 
-# p = soup.find('p')
-# p = soup.p['class']
-# a = soup.a['href']
-# for div in soup.find_all('div'):
-#   if div.get('id') == 'item-5e4184b6e6d48704f1813b9b':
-#     listings = div
-restaurant = []
+# gets everything between the ()
+addressregx = re.compile(r'(?<=\().+(?=\))')
+
 with open('restaurants.csv', mode='w', newline='') as restaurants:
   rest_writer = csv.writer(restaurants, delimiter=',', quotechar='"')
   for i, listing in enumerate(soup.find_all('li')):
-    # restaurant.append(listing) # makes an array of listings
-    # print(listing)
     name = listing.a.string
     link = listing.a.get('href')
-    text = listing.p.contents[1]
-    print(text)
-    rest_writer.writerow([name, link, text])
+    text = ''
+    if len(listing.p.contents) > 1:
+      text = listing.p.contents[1]
+    addressmatch = addressregx.search(text)
+    address = addressmatch.group(0)
+    print(address)
+    rest_writer.writerow([name, link, address])
+
+# [^\(].+(?=\))
+# [?<=\w].+(?=\))
+# (?<=\().+(?=\))
