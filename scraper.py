954f80c4c7b1304b5b42355746169f008d1587a8
Trevor Specht <trevor.specht@mapbox.com>
2020-11-02 19:02:29 -0500

    clean up, add comments

--- scraper.py
+++ scraper.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import csv
 import re
 
+# get web page and parse html into soup object
 url = 'https://www.willdrinkfortravel.com/posts/black-owned-restaurants-you-need-to-visit-in-baltimore'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "lxml")
@@ -16,19 +17,23 @@ descregex = re.compile(r'(?<=\) ).+')
 
 with open('restaurants.csv', mode='w', newline='') as restaurants:
   rest_writer = csv.writer(restaurants, delimiter=',', quotechar='"')
+  text = ''
+  address = ''
+  description = ''
+  # iterates over <li> tags
   for i, listing in enumerate(soup.find_all('li')):
+    # parse name and link from <a> tags
     name = listing.a.string
     link = listing.a.get('href')
-    text = ''
-    address = ''
-    description = ''
+    # isolate text in <p> tags
     if len(listing.p.contents) > 1:
-      text = listing.p.contents[1]
+      text = str(listing.p.contents[1])
+    # isolate address
     addressmatch = addressregex.search(text)
     if addressmatch:
       address = addressmatch.group(0)
+    # isolate description
     descmatch = descregex.search(text)
     if descmatch:
       description = descmatch.group(0)
-    print(description)
     rest_writer.writerow([name, link, address, description])
